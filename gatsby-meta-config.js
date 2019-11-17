bad7522363a4da689331880062cb6f369a4af045
Fleta <yasoueisin@gmail.com>
2019-11-18 01:55:41 +0900

    change: meta config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `FLETA.log`,
+  description: `Software, subcultures, etc.`,
+  author: `Gyeongeon Yun`,
+  introduction: `소프트웨어, 서브컬쳐 등 취미 이야기를 주로 합니다.`,
+  siteUrl: `https://blog.yuigahama.moe`, // Your blog site url
   social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
-    medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
+    twitter: `fletadev`,
+    github: `Fleta`,
+    medium: ``,
+    facebook: ``,
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'Fleta/fleta.log', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
